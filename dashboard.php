8806e6a35c24effd6a09b9a643534aa508a6f9b0
sitizahrafazria <stzhrafzria@gmail.com>
2025-11-07 12:04:57 +0700

    Dashboard Penjualan (perhitungan total)

--- dashboard.php
+++ dashboard.php
@@ -172,6 +172,7 @@ if (!isset($_SESSION['username'])) {
       $beli = rand(1, 10);
       $id_barang = rand(0, $jumlah);
       $harga = $harga_barang[$i] * $beli;
+      $grandtotal += $total;
     }
     ?>
   </main>
